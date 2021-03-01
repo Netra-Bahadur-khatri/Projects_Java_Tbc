09d6433c46422d21122c9f9e0fa659caa3f13f05
Netra Kc <71728112+Netra-Bahadur-khatri@users.noreply.github.com>
2021-03-02 00:58:55 +0545

    Add files via upload

---
+++ condStat_Mcq_Book/Exercise_4_1.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+public class Exercise_4_1 {
+    // int x = 1;
+    public static void main(String[] args) {
+        /*
+        What will be the minorOfThree Program if two or more of the values are equal ? if exactly two of the values are equal, does it matter whether the equal values are lower or higher than the third?
+        */
+
+
+        // System.out.println("x is " + x); 
+        // ans: compilation error
+
+        // int firstNum = 25;
+        // int seconNum = 3;
+        // double result = 25/3;
+        // System.out.println(result);
+
+;
+
+        System.out.println("The output is: ");
+        int index = 2;
+        index++;
+        System.out.println("index is " + index);
+
+
+ 
+
+ 		
+
+
+
+
+        
+
+    }
+}
+
+
+
